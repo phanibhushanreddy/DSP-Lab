2f78f28986fabb19834527978a594c9c618a9eba
phanibhushanreddy <159218788+phanibhushanreddy@users.noreply.github.com>
2024-08-26 15:41:26 +0300

    Update downsampling.py

--- Week - 1/downsampling.py
+++ Week - 1/downsampling.py
@@ -1,14 +1,8 @@
-
 from scipy.io import wavfile
-
 fs, x = wavfile.read('/home/apiiit123/Desktop/Week - 1/n.wav')
-
-
 def sampling(x, a):
     if a > 1:
         y=x[::a]
         wavfile.write('n.wav',fs,y)
-
-
 a=int(input("Enter sampling factor:"))
 sampling(x, a)
